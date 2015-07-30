dffa2b73ad95c864193d11269868dbea754140a2
Frank Horowitz <frank@horow.net>
2015-07-30 14:24:37 -0400

    Drop into debugger...

--- LevelJoiner.py
+++ LevelJoiner.py
@@ -213,7 +213,7 @@ for p,p_lon,p_lat in eq_query.filter(AppBasinEQs._depth_km_ != 0.).order_by(AppB
     if len(wq) == 0:
     	print "No Worms Nearby."
     	continue
-    
+    import pdb; pdb.set_trace()
     # N.B. if we index into all_worm_data with wq, we get an *ARRAY* of results
     # The rows of which are the things being indexed, while the first column is a WormPoint
     # and the second column is a WormLevelPoints. all_worm_data[wq][:,1]
