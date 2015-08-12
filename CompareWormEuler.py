0d4cf0558c3e5f03ff676b4042fae0a7fc88358d
Korin Carpenter <kcarpen3@u.rochester.edu>
2015-08-12 12:43:46 -0400

    Fixed a naming convention issue

--- CompareWormEuler.py
+++ CompareWormEuler.py
@@ -229,9 +229,9 @@ for p in euler_query:
     #print p._latitude_, p._longitude_, p._depth_km_, p._magnitude_
     
     # depth must be in meters!
-    eq_pt = p.geom
+    euler_pt = p.geom
     
-    dq,wq = worm_kd.query(eq_pt,k=20,distance_upper_bound=r)
+    dq,wq = worm_kd.query(euler_pt,k=20,distance_upper_bound=r)
     if (wq == end_idx).all():
         print "No Worms within %f meters."%r
         continue
