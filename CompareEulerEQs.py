26cd77aa42474f948b824e8e5049531cd158f31e
Korin Carpenter <kcarpen3@u.rochester.edu>
2015-08-20 13:20:37 -0400

    Looking at earthquake depths

--- CompareEulerEQs.py
+++ CompareEulerEQs.py
@@ -73,7 +73,7 @@ euler_query = session.query(ADKBGAEuler).filter(ADKBGAEuler.depth <= 7500.)
 euler_pt_coords = np.array([[e.xeuler,e.yeuler,e.depth] for e in euler_query])
 
 # Creating scikit-learn KDTree to speed up earthquake-euler point comparison
-euler_kd = neighbors.KDTree(euler_pt_coords,leaf_size=100)
+#euler_kd = neighbors.KDTree(euler_pt_coords,leaf_size=100)
 
 eq_query = session.query(ADKMergedEQs,
                          func.ST_Transform(ADKMergedEQs.geom,32618).ST_X(),
@@ -86,22 +86,26 @@ r = 10000.
 
 
 min_dist_to_nodes = []
+eq_depths = []
 	
-for p,p_lon,p_lat in eq_query.filter(ADKMergedEQs._Depth_km_ <= 7.5):
+for p,p_lon,p_lat in eq_query:
+#.filter(ADKMergedEQs._Depth_km_ <= 7.5):
     
     # depth must be in meters!
     eq_pt = [p_lon,p_lat,1000.*p._Depth_km_]
     
     # New scikit_learn.neighbors implementation of the query
-    wq,dq = euler_kd.query_radius(eq_pt,r=r,return_distance = True,sort_results=True)
+    #wq,dq = euler_kd.query_radius(eq_pt,r=r,return_distance = True,sort_results=True)
     
     # Displays earthquakes outside the range
-    if wq[0].shape[0] == 0:
-        print "No Euler points within %f meters."%r
-        continue
-    min_dist_to_nodes += [dq[0][0]]
+    #if wq[0].shape[0] == 0:
+    #    print "No Euler points within %f meters."%r
+    #    continue
+    #min_dist_to_nodes += [dq[0][0]]
     
-    sys.stdout.flush()
+    eq_depths += p._Depth_km_
+    
+    #sys.stdout.flush()
     #print 'NEW EARTHQUAKE'
     
 print "Done"
